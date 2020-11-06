7e5d011d4bfbccaa53e1260e2a4ccaef5020b8d4
Владимир Зейналов <99percent6@gmail.com>
2020-11-06 09:14:54 +0400

    add cross origin host

--- src/main/java/com/test/сurrency/controller/CurrencyRestController.java
+++ src/main/java/com/test/сurrency/controller/CurrencyRestController.java
@@ -11,16 +11,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @Slf4j
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping(value = "/v1/currency", produces = "application/json")
 public class CurrencyRestController {
 
